e59ee478a634133ca50c9489ac7d8c995eb62ab4
Andrew Azores <aazores@redhat.com>
2024-08-12 17:30:07 -0400

    sooner, clearer failure if GitHub PR title is not as expected

--- src/main/java/com/github/andrewazores/GitHubIntegration.java
+++ src/main/java/com/github/andrewazores/GitHubIntegration.java
@@ -26,8 +26,8 @@ import jakarta.inject.Inject;
 class GitHubIntegration implements SourceIntegration {
     private static final Pattern GH_PR_TITLE_PATTERN =
             Pattern.compile(
-                    "^build\\(deps\\): bump (?<ga>[a-z0-9:._-]+) from (?:[a-z0-9._-]+) to"
-                            + " (?<version>[a-z0-9._-]+)$",
+                    "^build\\(deps\\): bump (?<ga>[a-z0-9._-]+:[a-z0-9._-]+) from (?:[a-z0-9._-]+)"
+                            + " to (?<version>[a-z0-9._-]+)$",
                     Pattern.MULTILINE);
 
     @Inject CliSupport cli;
@@ -47,9 +47,17 @@ class GitHubIntegration implements SourceIntegration {
         if (!matcher.matches()) {
             throw new RuntimeException(
                     String.format(
-                            "GitHub PR URL \"%s\" was not understandable. Got title: \"%s\"",
+                            "GitHub PR URL \"%s\" was not understandable. Got title: \"%s\". Is"
+                                + " this a Dependabot Pull Request? Does the title contain a single"
+                                + " GroupId:ArtifactId or a Maven property for upgrading a"
+                                + " dependency group?",
                             url, proc.out().get(0)));
         }
-        return String.format("%s:%s", matcher.group("ga"), matcher.group("version"));
+        var ga = matcher.group("ga");
+        var version = matcher.group("version");
+        Log.debugv(
+                "Interpreted GitHub PR title \"{0}\" as request for {1}:{2}",
+                proc.out().get(0), ga, version);
+        return String.format("%s:%s", ga, version);
     }
 }
