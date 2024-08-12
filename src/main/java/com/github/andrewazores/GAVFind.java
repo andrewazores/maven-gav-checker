eee6797caaf06a52d94e241684f79bba1763259d
Andrew Azores <aazores@redhat.com>
2024-08-12 13:48:19 -0400

    set up as Maven project

--- src/main/java/com/github/andrewazores/GAVFind.java
+++ src/main/java/com/github/andrewazores/GAVFind.java
@@ -1,15 +1,32 @@
-///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS info.picocli:picocli:4.6.3
+/// usr/bin/env jbang "$0" "$@" ; exit $?
+// DEPS info.picocli:picocli:4.6.3
+/*
+ * Copyright Andrew Azores.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.andrewazores;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
@@ -19,43 +36,70 @@ import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
-
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import picocli.CommandLine.Help.Ansi;
 
-@Command(name = "GAVFind", mixinStandardHelpOptions = true, version = "GAVFind 0.1",
+@Command(
+        name = "GAVFind",
+        mixinStandardHelpOptions = true,
+        version = "GAVFind 0.1",
         description = "Check Maven dependencies' availability in a particular Maven repository")
 public class GAVFind implements Callable<Integer> {
 
-    private static final Pattern GAV_PATTERN = Pattern.compile("^(?<group>[a-z0-9._-]+):(?<artifact>[a-z0-9._-]+)(?::(?<version>[a-z0-9._-]+))?$", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
-    private static final Pattern GH_PR_TITLE_PATTERN = Pattern.compile("^build\\(deps\\): bump (?<ga>[a-z0-9:._-]+) from (?:[a-z0-9._-]+) to (?<version>[a-z0-9._-]+)$", Pattern.MULTILINE);
+    private static final Pattern GAV_PATTERN =
+            Pattern.compile(
+                    "^(?<group>[a-z0-9._-]+):(?<artifact>[a-z0-9._-]+)(?::(?<version>[a-z0-9._-]+))?$",
+                    Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+    private static final Pattern GH_PR_TITLE_PATTERN =
+            Pattern.compile(
+                    "^build\\(deps\\): bump (?<ga>[a-z0-9:._-]+) from (?:[a-z0-9._-]+) to"
+                            + " (?<version>[a-z0-9._-]+)$",
+                    Pattern.MULTILINE);
 
-    @Parameters(index = "0", description = "The Maven dependency GroupId:ArtifactId[:Version] (GAV), ex. org.slf4j:slf4j-api:2.0.12 or info.picocli:picocli . If no version is specified (version listing mode) then all available versions are printed, otherwise the existence of the specified version is checked. If the GitHub 'gh' client is installed, this can also be a GitHub Pull Request path and the PR title will be used to infer the GAV.")
+    @Parameters(
+            index = "0",
+            description =
+                    "The Maven dependency GroupId:ArtifactId[:Version] (GAV), ex."
+                        + " org.slf4j:slf4j-api:2.0.12 or info.picocli:picocli . If no version is"
+                        + " specified (version listing mode) then all available versions are"
+                        + " printed, otherwise the existence of the specified version is checked."
+                        + " If the GitHub 'gh' client is installed, this can also be a GitHub Pull"
+                        + " Request path and the PR title will be used to infer the GAV.")
     private String gav;
 
-    @Option(names = { "-r", "--repository" }, description = "The Maven repository root URL to search, ex. https://repo.maven.apache.org/maven2/", defaultValue = "https://repo.maven.apache.org/maven2/")
+    @Option(
+            names = {"-r", "--repository"},
+            description =
+                    "The Maven repository root URL to search, ex."
+                            + " https://repo.maven.apache.org/maven2/",
+            defaultValue = "https://repo.maven.apache.org/maven2/")
     private String repoRoot;
 
-    @Option(names = { "-c", "--count" }, description = "The number of release versions to list in version listing mode. Defaults to the full list", defaultValue = "-1")
+    @Option(
+            names = {"-c", "--count"},
+            description =
+                    "The number of release versions to list in version listing mode. Defaults to"
+                            + " the full list",
+            defaultValue = "-1")
     private int count;
 
-    @Option(names = { "-k", "--insecure" }, description = "Disable TLS validation", defaultValue = "false")
+    @Option(
+            names = {"-k", "--insecure"},
+            description = "Disable TLS validation",
+            defaultValue = "false")
     private boolean insecure;
 
-    @Option(names = { "-v", "--verbose" }, description = "Enable verbose debugging output", defaultValue = "false")
+    @Option(
+            names = {"-v", "--verbose"},
+            description = "Enable verbose debugging output",
+            defaultValue = "false")
     private boolean verbose;
 
     public static void main(String... args) {
@@ -84,12 +128,21 @@ public class GAVFind implements Callable<Integer> {
         var version = matcher.group("version");
         boolean exactMatch = !(version == null || "null".equals(version));
         if (exactMatch) {
-            System.out.println(String.format("Searching %s for version \"%s\" of %s from %s", repoRoot, version, artifactId, groupId));
+            System.out.println(
+                    String.format(
+                            "Searching %s for version \"%s\" of %s from %s",
+                            repoRoot, version, artifactId, groupId));
         } else {
-            System.out.println(String.format("Searching %s for available versions of %s from %s", repoRoot, artifactId, groupId));
+            System.out.println(
+                    String.format(
+                            "Searching %s for available versions of %s from %s",
+                            repoRoot, artifactId, groupId));
         }
 
-        var url = String.format("%s/%s/%s/maven-metadata.xml", repoRoot, groupId.replaceAll("\\.", "/"), artifactId);
+        var url =
+                String.format(
+                        "%s/%s/%s/maven-metadata.xml",
+                        repoRoot, groupId.replaceAll("\\.", "/"), artifactId);
         var factory = DocumentBuilderFactory.newDefaultInstance();
         var documentBuilder = factory.newDocumentBuilder();
         if (verbose) {
@@ -106,38 +159,48 @@ public class GAVFind implements Callable<Integer> {
         if (exactMatch) {
             var versionMatch = versioning.contains(version);
             if (versionMatch.isPresent()) {
-                System.out.println(String.format("%s:%s:%s is available as %s in %s", groupId, artifactId, version, versionMatch.get(), repoRoot));
+                System.out.println(
+                        String.format(
+                                "%s:%s:%s is available as %s in %s",
+                                groupId, artifactId, version, versionMatch.get(), repoRoot));
             } else {
-                System.err.println(String.format("%s:%s:%s is NOT available in %s", groupId, artifactId, version, repoRoot));
+                System.err.println(
+                        String.format(
+                                "%s:%s:%s is NOT available in %s",
+                                groupId, artifactId, version, repoRoot));
                 System.err.println("available:");
-                versioning.versions().stream().limit(count < 0 ? Integer.MAX_VALUE : count).map(v -> "\t" + v).forEach(System.err::println);
+                versioning.versions().stream()
+                        .limit(count < 0 ? Integer.MAX_VALUE : count)
+                        .map(v -> "\t" + v)
+                        .forEach(System.err::println);
                 return 2;
             }
         } else {
-            System.out.println(String.format("latest: %s\n", versioning.latest()));
-            System.out.println(String.format("release: %s\n", versioning.release()));
+            System.out.println(String.format("latest: %s%n", versioning.latest()));
+            System.out.println(String.format("release: %s%n", versioning.release()));
             System.out.println("available:");
-            versioning.versions().stream().limit(count < 0 ? Integer.MAX_VALUE : count).map(v -> "\t" + v).forEach(System.out::println);
+            versioning.versions().stream()
+                    .limit(count < 0 ? Integer.MAX_VALUE : count)
+                    .map(v -> "\t" + v)
+                    .forEach(System.out::println);
         }
 
         return 0;
     }
 
     private void disableTlsValidation() throws NoSuchAlgorithmException, KeyManagementException {
-        TrustManager trm = new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
+        TrustManager trm =
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
 
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
 
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        };
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                };
         SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, new TrustManager[] { trm }, null);
+        sc.init(null, new TrustManager[] {trm}, null);
         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 
@@ -146,7 +209,11 @@ public class GAVFind implements Callable<Integer> {
         var latest = getChild(versioning, "latest").orElseThrow().getTextContent();
         var release = getChild(versioning, "release").orElseThrow().getTextContent();
         var versions = getChild(versioning, "versions").orElseThrow();
-        var versionList = new ArrayList<>(getChildren(versions, "version").stream().map(Node::getTextContent).toList());
+        var versionList =
+                new ArrayList<>(
+                        getChildren(versions, "version").stream()
+                                .map(Node::getTextContent)
+                                .toList());
         Collections.reverse(versionList);
 
         return new Versioning(latest, release, Collections.unmodifiableList(versionList));
@@ -189,7 +256,7 @@ public class GAVFind implements Callable<Integer> {
 
     private static boolean versionCompare(String request, String found) {
         return found.startsWith(String.format("%s-", request))
-            || found.startsWith(String.format("%s.", request));
+                || found.startsWith(String.format("%s.", request));
     }
 
     private String extractGitHubTitleGav(String url) throws IOException, InterruptedException {
@@ -203,7 +270,10 @@ public class GAVFind implements Callable<Integer> {
         }
         var matcher = GH_PR_TITLE_PATTERN.matcher(proc.out().get(0));
         if (!matcher.matches()) {
-            throw new RuntimeException(String.format("GitHub PR URL \"%s\" was not understandable. Got title: \"%s\"", url, proc.out().get(0)));
+            throw new RuntimeException(
+                    String.format(
+                            "GitHub PR URL \"%s\" was not understandable. Got title: \"%s\"",
+                            url, proc.out().get(0)));
         }
         return String.format("%s:%s", matcher.group("ga"), matcher.group("version"));
     }
